8a2646841cbc892ad85f2e8f6376b9a9e1cf88e4
Rodrigo <rodrigo_sique@hotmail.com>
2026-09-23 17:35:21 -0300

    Inicializando projeto Spring Boot

---
+++ src/main/java/com/rodrigosiqueira_pedidos/projeto_pedidos/ProjetoPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigosiqueira_pedidos.projeto_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigosiqueira_pedidos/projeto_pedidos/ProjetoPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigosiqueira_pedidos.projeto_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
